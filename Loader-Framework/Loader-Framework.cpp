5c0f9287bdbe6e308e3240f558d0b87b51ba7c39
relisted <relisted@protonmail.com>
2021-01-27 14:55:38 +0100

    added xor, all strings are now thru xor

--- Loader-Framework/Loader-Framework.cpp
+++ Loader-Framework/Loader-Framework.cpp
@@ -1,33 +1,34 @@
 #include "classes.h"
 #include "downloader.h"
 #include "utils.h"
+#include "xor.h"
 
 int main() {
-	DebugMessage("WARNING: Debug mode\n\n");
+	DebugMessage(xorstr_("WARNING: Debug mode\n\n"));
 
-	Menu menu("Title");
+	Menu menu(xorstr_("Title"));
 
 	std::string key_input;
 	std::cin >> key_input;
 
 	if (!ping_new(PING_DEST)) {
-		std::cout << "[-] Website not responding, exiting.\n";
+		std::cout << xorstr_("[-] Website not responding, exiting.\n");
 		pause_until_leave();
 	}
 
-	if (!is_key_allowed_insecure(key_input)) { menu.add_item_string("wrong key."); pause_until_leave(); }
-	else { menu.add_item_string("key correct."); }
+	if (!is_key_allowed_insecure(key_input)) { menu.add_item_string(xorstr_("wrong key.")); pause_until_leave(); }
+	else { menu.add_item_string(xorstr_("key correct.")); }
 
-	std::cout << "[+] Website up, proceeding\n";
+	std::cout << xorstr_("[+] Website up, proceeding\n");
 
-	std::cout << "[*] Downloading...\n";
+	std::cout << xorstr_("[*] Downloading...\n");
 
 	if (download_file_from_url(FILE_SOURCE, get_path())) {
-		std::cout << "[+] File downloaded and stored in: " << get_path() << "\n";
+		std::cout << xorstr_("[+] File downloaded and stored in: ") << get_path() << "\n";
 		pause_until_leave();
 	} 
 	else {
-		std::cout << "[-] Couldn't download file\n";
+		std::cout << xorstr_("[-] Couldn't download file\n");
 		pause_until_leave();
 	}
 
--- Loader-Framework/classes.cpp
+++ Loader-Framework/classes.cpp
@@ -8,11 +8,11 @@ Menu::Menu(std::string title) {
 };
 
 void Menu::add_item_int(const int& input_int) {
-	std::cout << "\n" << "integer value: " << input_int << "\n";
+	std::cout << "\n" << xorstr_("integer value: ") << input_int << "\n";
 };
 void Menu::add_item_float(const float& input_float) {
-	std::cout << "\n" << "float value: " << input_float << "\n";
+	std::cout << "\n" << xorstr_("float value: ") << input_float << "\n";
 };
 void Menu::add_item_string(const std::string& input_str) {
-	std::cout << "\n" << "string value: " << input_str << "\n";
+	std::cout << "\n" << xorstr_("string value: ") << input_str << "\n";
 };
