466292b706bf9daf7f8755c473e1d7f25e09b25a
RelistedWasTaken <73892791+RelistedWasTaken@users.noreply.github.com>
2021-01-30 23:24:27 +0100

    Update includes.h

--- Loader-Framework/includes.h
+++ Loader-Framework/includes.h
@@ -3,11 +3,10 @@
 #include "xor.h"
 #include <iostream>
 
-#define FILE_SOURCE xorstr_("https://download.ccleaner.com/cctrialsetup.exe")
-#define KEY_LOCATION xorstr_("http://somecoolwebsiteiuse.tk/file.txt")
-#define PING_DEST xorstr_("http://somecoolwebsiteiuse.tk")
+#define FILE_SOURCE xorstr_("http://insert_website_name_here.com/file_name.exe")
+#define KEY_LOCATION xorstr_("http://insert_website_name_here.com/key.txt")
+#define PING_DEST xorstr_("http://insert_website_name_here.com")
 
 //FILE_SOURCE http://insert_website_name_here.com/file_name.exe
 //KEY_LOCATION http://insert_website_name_here.com/key.txt (doesnt have to be .txt, you can just upload it to your github as a .txt LOL)
 //PING_DEST http://insert_website_name_here.com
-//http://somecoolwebsiteiuse.tk/file.txt
