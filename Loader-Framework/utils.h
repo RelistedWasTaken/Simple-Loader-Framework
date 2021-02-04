303c4625e4eac72ff73b4ac83ab7e74ffc262aa9
RelistedWasTaken <73892791+RelistedWasTaken@users.noreply.github.com>
2021-02-04 17:17:02 +0100

    Update utils.h

--- Loader-Framework/utils.h
+++ Loader-Framework/utils.h
@@ -6,10 +6,6 @@
 #include <filesystem>
 #include <vector>
 
-int t_;
-int f_;
-int* result;
-
 std::string get_user_name() {
 	TCHAR username[UNLEN + 1];
 	DWORD size = UNLEN + 1;
@@ -45,37 +41,6 @@ bool is_key_allowed_insecure(const std::string& input_sent) {
 	}
 	return false;
 }
-
-int is_key_allowed_secure(const std::string& input_sent) {
-	std::vector<std::string> keys_fetched = fetch_all_info_from_txt();
-	for (auto& key : keys_fetched) {
-		t_ = rand() % 1651541681 + 1;
-		f_ = rand() % 2581517441 + 1;
-		if (key == input_sent) {
-			result = new int(t_ + f_);
-			return t_;
-		}
-	}
-	result = new int(t_ - f_);
-	return f_;
-}
-
-/*
-* secure vs non-secure
-*	non-secure
-*	returns a boolean value which can easily be faked.
-*	usage:
-*	if (!is_key_allowed_insecure(key_input)) { menu.add_item_string("wrong key."); return 0; }
-*	else { menu.add_item_string("key correct."); }
-*	
-*	secure
-*	returns a random value and compares it after the end
-*	usage:
-*	is_key_allowed_secure(key_input)
-*	if (*result == t_ - f_) { menu.add_item_string("wrong key."); return 0; }
-*	else if (*result == t_ + f_) { menu.add_item_string("key correct."); }
-*/
-
 
 //thx dupecc
 void open_app_insecure(std::string path, LPCSTR arguments, bool hidden) {
